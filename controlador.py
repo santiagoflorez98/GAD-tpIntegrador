82fe728371d086e485dd252fbbb98854b8db0593
Santiago Florez <santiagoo98p@gmail.com>
2023-02-10 19:07:35 -0300

    feat: estadisticas de coincidencias

--- controlador.py
+++ controlador.py
@@ -26,21 +26,33 @@ def consulta(ruta, radio):
 def contarCoincidencias(rutaOr, rutaCopia):
     primeraCoincidencia = False
     coincidencias = 0
-    resOriginal = consulta(rutaOr,1)
-    resCopia = consulta(rutaCopia,1)
-    for i in range(0,5):
+    resOriginal = consulta(rutaOr,0.3)
+    resCopia = consulta(rutaCopia,0.3)
+    tamañoOriginal = len(resOriginal)
+    tamañoCopia = len(resCopia)
+    i = 0
+    while (i < 5 | i < tamañoOriginal):
         j = 0
-        while ((resOriginal[i][2] != resCopia[j][2]) & (j<4)):
+        while ((resOriginal[i][2] != resCopia[j][2]) & (j<4 | j<tamañoCopia)):
             j += 1
         if resOriginal[i][2] == resCopia[j][2]:
             coincidencias +=1
             if (i == 0):
                 primeraCoincidencia = True
+        i += 1
     return coincidencias,primeraCoincidencia
 
 def consultas():
+    acumuladorCoincidencias = 0
+    contadorPrimeraCoincidencia = 0
     for i in range(1,51):
         res = contarCoincidencias(f'C:/Users/santi/OneDrive/Escritorio/consultasOriginales/{i}.png'
                             ,f'C:/Users/santi/OneDrive/Escritorio/consultasOtras/{i}.png')
         print (f'Imagen {i} - numero de coincidencias: {res[0]}')
-        if (res[1]): print('hubo coincidencia en la primera imagen')
+        acumuladorCoincidencias += res[0]
+        if (res[1]):
+            print('hubo coincidencia en la primera imagen')
+            contadorPrimeraCoincidencia +=1
+    porcentajePrimeraCoincidencia = (contadorPrimeraCoincidencia / 50) * 100
+    porcentajeCoincidencias = (acumuladorCoincidencias / 250) * 100
+    return porcentajeCoincidencias,porcentajePrimeraCoincidencia
--- main.py
+++ main.py
@@ -2,5 +2,7 @@ import controlador
 import interfaz
 
 #interfaz.ventanaPrincipal()
-#cargarEnDb()
-controlador.consultas()
+#controlador.cargarEnDb()
+resultado = controlador.consultas()
+print(f'Porcentaje de acierto primera coincidencia: {resultado[0]}')
+print(f'Porcentaje de acierto primeras 5 coincidencias: {resultado[1]}')
